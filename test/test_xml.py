0309409e34fe0a1b1b1e79e7e81c1bd2cad55c03
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-01-15 20:13:54 -0700

    fix test

--- test/test_xml.py
+++ test/test_xml.py
@@ -16,5 +16,21 @@ def test_xml_constructor():
       xmlns:isbn='urn:ISBN:0-395-36341-6'>:
     <isbn:number>: 1568491379
 '''
-    desired = '''<div xmlns="urn:loc.gov:books" xmlns:isbn="urn:ISBN:0-395-36341-6"></div>\n'''
+    desired = '''<book xmlns="urn:loc.gov:books" xmlns:isbn="urn:ISBN:0-395-36341-6">
+    <isbn:number>1568491379</isbn:number>
+</book>
+'''
+    assert_output(input, desired)
+
+
+def test_xml_self_close():
+    input = '''
+<book xmlns='urn:loc.gov:books',
+      xmlns:isbn='urn:ISBN:0-395-36341-6'>:
+    <isbn:number>
+'''
+    desired = '''<book xmlns="urn:loc.gov:books" xmlns:isbn="urn:ISBN:0-395-36341-6">
+    <isbn:number />
+</book>
+'''
     assert_output(input, desired)
