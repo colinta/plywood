491436d0ead7f504f916ddc92cca408a9b6e7221
Colin T.A. Gray <colinta@gmail.com>
2013-05-01 12:39:39 -0600

    added 'len'

--- test/test_additions.py
+++ test/test_additions.py
@@ -6,21 +6,32 @@ def test_reverse():
 test = [1,2,3]
 for t in reverse(test):
     t
+for t in test|reverse:
+    t
 '''
     desired = '''3
+2
+1
+3
 2
 1
 '''
     assert_output(input, desired)
 
 
+def test_len():
+    input = 'len([1,2,3])\n"test"|len'
+    desired = '3\n4\n'
+    assert_output(input, desired)
+
+
 def test_cdata():
-    input = 'cdata("content")'
-    desired = '<![CDATA[content]]>\n'
+    input = 'cdata("content")\n"content"|cdata'
+    desired = '<![CDATA[content]]>\n<![CDATA[content]]>\n'
     assert_output(input, desired)
 
 
 def test_e():
-    input = 'e("<>&“”")'
-    desired = '&lt;&gt;&amp;&ldquo;&rdquo;\n'
+    input = 'e("<>&“”")\n"<>&“”" | e'
+    desired = '&lt;&gt;&amp;&ldquo;&rdquo;\n&lt;&gt;&amp;&ldquo;&rdquo;\n'
     assert_output(input, desired)
